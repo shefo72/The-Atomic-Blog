541b3f4dd28d3c7ba1609356f98dd6073d5414df
Ahmed Sherif <ahmedsherifeg22@gmail.com>
2025-12-14 18:43:21 +0200

    Separate React Context

---
+++ src/PostContext.js
@@ -0,0 +1,51 @@
+import { useState, createContext, useContext } from "react";
+import createRandomPost from "./createRandomPost";
+
+const PostContext = createContext();
+
+function PostProvider({ children }) {
+  const [posts, setPosts] = useState(() =>
+    Array.from({ length: 30 }, () => createRandomPost())
+  );
+  const [searchQuery, setSearchQuery] = useState("");
+  // Derived state. These are the posts that will actually be displayed
+
+  const searchedPosts =
+    searchQuery.length > 0
+      ? posts.filter((post) =>
+          `${post.title} ${post.body}`
+            .toLowerCase()
+            .includes(searchQuery.toLowerCase())
+        )
+      : posts;
+
+  function handleAddPost(post) {
+    setPosts((posts) => [post, ...posts]);
+  }
+
+  function handleClearPosts() {
+    setPosts([]);
+  }
+
+  return (
+    <PostContext.Provider
+      value={{
+        posts: searchedPosts,
+        onClearPosts: handleClearPosts,
+        searchQuery,
+        setSearchQuery,
+        handleAddPost,
+      }}
+    >
+      {children}
+    </PostContext.Provider>
+  );
+}
+
+function usePosts() {
+  const context = useContext(PostContext);
+  if (context === undefined)
+    throw new Error("PostContext was used outside of the PostProvider");
+  return context;
+}
+export { PostProvider, usePosts };
